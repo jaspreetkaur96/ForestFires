cbd8b5bdfe48d6eb21bd872357a91adf6ce79bed
jae1015 <jaspreet.k.c1996@gmail.com>
2018-01-31 00:54:11 +0530

    mean,median,range,quartiles,IQR

--- stats.py
+++ stats.py
@@ -1,11 +1,14 @@
 import numpy as np
 import pandas as pd
+from prettytable import PrettyTable
 
 df=pd.read_csv("forestfires.csv")
 
-for col in df:
-	print "mean ",col,np.mean(df[col])
-	print "median",col,np.median(df[col])
-	a=min(df[col])
-	b=max(df[col])
-	print "range",col," ",a,",",b
+def IQR(col):
+    return np.percentile(df[col], 75) - np.percentile(df[col], 25)
+
+#------------------------------------------------------------------------------------------------------
+t = PrettyTable(['Mean', 'Median','Min','Max','1st Quar','3rd Quar','IQR'])
+
+	t.add_row([np.mean(df[col]),np.median(df[col]),min(df[col]),max(df[col]),np.percentile(df[col],25),np.percentile(df[col],75),IQR(col)])
+print t
