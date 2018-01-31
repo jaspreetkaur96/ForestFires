e1b16b75db40fc9ce093d5deb54c6ed88b8d5ccf
jae1015 <jaspreet.k.c1996@gmail.com>
2018-01-31 22:58:54 +0530

    mean,median,range,IQR

--- stats.py
+++ stats.py
@@ -9,6 +9,7 @@ def IQR(col):
 
 #------------------------------------------------------------------------------------------------------
 t = PrettyTable(['Mean', 'Median','Min','Max','1st Quar','3rd Quar','IQR'])
-
+	
+for col in df:
 	t.add_row([np.mean(df[col]),np.median(df[col]),min(df[col]),max(df[col]),np.percentile(df[col],25),np.percentile(df[col],75),IQR(col)])
 print t
